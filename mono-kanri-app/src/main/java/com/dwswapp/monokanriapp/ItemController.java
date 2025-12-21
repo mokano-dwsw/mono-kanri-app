fc8b9b5e35b22f4129833a23c02c6b487dfc42f2
M.Okano <mokano.dwsw@gmail.com>
2025-12-21 21:11:32 +0900

    fix: import warning

--- mono-kanri-app/src/main/java/com/dwswapp/monokanriapp/ItemController.java
+++ mono-kanri-app/src/main/java/com/dwswapp/monokanriapp/ItemController.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping; // ★ 追加
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;  // ★ 追加import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
